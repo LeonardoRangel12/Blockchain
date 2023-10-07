494fb11ef8beef537fc1394ea52226cc9af48809
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2023-10-06 19:55:58 -0700

    Router añadido a app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,12 +2,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/app.css";
 import $ from "jquery";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// Todas las plantillas HTML
 import header from "./components/header";
 import footer from "./components/footer";
+import home from "./components/home";
 import user from "./components/user";
 const app = async () => {
     $("#header").html(header());
-    $("#user").html(await user());
+    // ROUTER
+    if (window.location.pathname === "/" || window.location.pathname === "/home") {
+        $("#content").html(await home());    
+    }
+    else if(window.location.pathname === "/user"){
+        $("#content").html(await user());
+    }
+    else{
+        $("#content").html("<h1>404 Not Found</h1>");
+    }
+    
     $("#footer").html(footer());
 };
 
