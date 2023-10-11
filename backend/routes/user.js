9b0f3450d9c5c35bde45406f61c0512a5fde13d5
JulyAn1234 <l20211790@tectijuana.edu.mx>
2023-10-11 12:33:41 -0700

    change of names

--- backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../controllers/user');
-/* GET users listing. */
-router.get('/random', async function(req, res, next) {
-  res.send(await User.getRandomUser());
-});
-
-module.exports = router;
