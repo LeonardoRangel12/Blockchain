150c37cd568d7b3071a0c1d2bac732d13a5785db
repc2002 <l20212421@tectijuana.edu.mx>
2023-10-13 03:38:08 -0700

    Footer Finalizado

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,5 +1,7 @@
 const footer = async () => {
     const footer = `
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" rel="stylesheet">
+
     <footer class="bg-dark text-center text-white">
     <!-- Grid container -->
     <div class="container p-4 pb-0">
@@ -7,13 +9,13 @@ const footer = async () => {
       <section class="mb-4">        
         <!-- Twitter -->
         <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/HypirionGSprout" role="button"
-          ><i class="fab fa-twitter">Twitter</i
+          ><i class="fab fa-twitter"></i
         ></a>
 
       </section>
     </div>
       <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2023 Copyright
+      © 2023 Copyright Hyperon GSprout
     </div>
     <!-- Copyright -->
   </footer>`;
