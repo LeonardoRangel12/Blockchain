8e2d0a81c97b53d0616eb8baf205ccf7a12bb3ab
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2023-10-13 14:05:03 -0700

    Final

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,50 +5,39 @@ import $ from "jquery";
 
 import header from "./components/header";
 import footer from "./components/footer";
-import {connect} from "./components/connect";
+import { connect } from "./components/connect";
 import login from "./components/login";
 import signup from "./components/signup";
 import landingPage from "./components/landingPage";
 import gamePage from "./components/gamePage";
 import main from "./components/main";
 import searchPage from "./components/searchPage";
-import upload from './components/upload';
+import upload from "./components/upload";
 const app = async () => {
+  if (window.location.pathname === "/") {
+    $("#content").html(await landingPage());
+  } else {
+    $("#header").html(header());
 
-    if(window.location.pathname === "/")
-    {
-        $("#content").html(await landingPage());            
+    if (window.location.pathname === "/transferir") {
+      $("#content").html(await connect());
+    } else if (window.location.pathname === "/main") {
+      $("#content").html(await main());
+    } else if (window.location.pathname === "/gamePage") {
+      $("#content").html(await gamePage());
+    } else if (window.location.pathname === "/signup") {
+      $("#content").html(await signup());
+    } else if (window.location.pathname === "/login") {
+      $("#content").html(await login());
+    } else if (window.location.pathname === "/searchPage") {
+      $("#content").html(await searchPage());
+    } else if (window.location.pathname === "/upload") {
+      $("#content").html(await upload());
+    } else {
+      $("#content").html("404 Not Found");
     }
-    else
-    {
-        $("#header").html(header());
-
-        if (window.location.pathname === "/transferir" ) {
-            $("#content").html(await connect());
-        }
-        else if (window.location.pathname === "/main" ) {
-            $("#content").html(await main());
-        }
-        else if (window.location.pathname === "/gamePage" ) {
-            $("#content").html(await gamePage());
-        }
-        else if (window.location.pathname === "/signup" ) {
-            $("#content").html(await signup());
-        }
-        else if (window.location.pathname === "/login" ) {
-            $("#content").html(await login());
-        }
-        else if (window.location.pathname === "/searchPage" ) {
-            $("#content").html(await searchPage());
-        }
-        else if (window.location.pathname === "/subir" ) {
-            $("#content").html(await subir());
-        }
-        else{
-            $("#content").html("404 Not Found");
-        }
-        $("#footer").html(await footer());
-    }
+    $("#footer").html(await footer());
+  }
 };
 
 // Init app
