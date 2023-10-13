6cfad76a767d754ea8ec1f6785fd441da161f391
repc2002 <l20212421@tectijuana.edu.mx>
2023-10-12 23:39:07 -0700

    Prueba de Footers

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -6,7 +6,7 @@ const footer = () => {
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
       <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
     </a>
-
+      Hola Mundo!!
     <ul class="nav col-md-4 justify-content-end">
       <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
       <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
