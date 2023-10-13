31e35f264bbc654811ee35d715e166a5187ed431
repc2002 <l20212421@tectijuana.edu.mx>
2023-10-13 08:12:23 -0700

    Creacion de searchPage

--- frontend/src/components/searchPage.js
+++ frontend/src/components/searchPage.js
@@ -1,36 +1,33 @@
 import "../styles/search.css";
 const searchPage = async () => {
-    const searchPage = `    
-    <div class="video-container">
-        <video autoplay muted loop id="myVideo">
-            <!-- Reemplaza 'video.mp4' con la URL de tu video -->
-            <source src="../assets/Video.mp4" type="video/mp4">
-            Tu navegador no soporta el elemento de video.
-        </video>
-        
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-12 col-md-10 col-lg-8">
-                    <form class="card card-sm">
-                        <div class="card-body row no-gutters align-items-center">
-                            <div class="col-auto">
-                                <i class="fas fa-search h4 text-body"></i>
-                            </div>
-                            <div class="col">
-                                <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Search topics or keywords">
-                            </div>
-                            <div class="col-auto">
-                                <button class="btn btn-lg btn-success" type="submit">Search</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+  const searchPage = `    
+  <link
+  rel="stylesheet"
+  href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
+  integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
+  crossorigin="anonymous"
+/>
+<div class="d-flex justify-content-center align-items-center vh-100">
+  <form class="card card-sm">
+    <div class="card-body row no-gutters align-items-center">
+      <div class="col-auto">
+        <i class="fas fa-search h4 text-body"></i>
+      </div>
+      <div class="col">
+        <input
+          class="form-control form-control-lg form-control-borderless"
+          type="search"
+          placeholder="Search topics or keywords"
+        />
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-lg btn-success" type="submit">Search</button>
+      </div>
     </div>
-`;
+  </form>
+</div>`;
 
   return searchPage;
-}
+};
 
 export default searchPage;
