88f4d436eccadb1c9d3be92fb3b7e0cc70509632
LuisPantoja <l20211821@tectijuana.edu.mx>
2023-10-13 04:11:59 -0700

    registro corregido

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -66,25 +66,6 @@ const signup = async () => {
                     Sign up
                   </button>
     
-                  <!-- Register buttons -->
-                  <div class="text-center">
-                    <p>or sign up with:</p>
-                    <button type="button" class="btn btn-link btn-floating mx-1">
-                      <i class="fab fa-facebook-f"></i>
-                    </button>
-    
-                    <button type="button" class="btn btn-link btn-floating mx-1">
-                      <i class="fab fa-google"></i>
-                    </button>
-    
-                    <button type="button" class="btn btn-link btn-floating mx-1">
-                      <i class="fab fa-twitter"></i>
-                    </button>
-    
-                    <button type="button" class="btn btn-link btn-floating mx-1">
-                      <i class="fab fa-github"></i>
-                    </button>
-                  </div>
                 </form>
               </div>
             </div>
