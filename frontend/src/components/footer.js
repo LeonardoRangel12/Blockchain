5838142f16b9a9ca4f545725732426b479af448f
repc2002 <l20212421@tectijuana.edu.mx>
2023-10-13 03:28:06 -0700

    Cambios en footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -7,14 +7,13 @@ const footer = async () => {
       <section class="mb-4">        
         <!-- Twitter -->
         <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/HypirionGSprout" role="button"
-          ><i class="fab fa-twitter"></i
+          ><i class="fab fa-twitter">Twitter</i
         ></a>
 
       </section>
     </div>
       <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2023 Copyright:
-      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+      © 2023 Copyright
     </div>
     <!-- Copyright -->
   </footer>`;
