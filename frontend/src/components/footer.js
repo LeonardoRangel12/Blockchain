963b551ed1fb02f2e36f1e5c92f49fd603ad7448
repc2002 <l20212421@tectijuana.edu.mx>
2023-10-13 00:05:55 -0700

    Modificaciones footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,60 +1,37 @@
 const footer = async () => {
     const footer = `
-    <footer class="bg-light text-center text-white">
+    <footer class="bg-dark text-center text-white">
     <!-- Grid container -->
     <div class="container p-4 pb-0">
       <!-- Section: Social media -->
       <section class="mb-4">
         <!-- Facebook -->
-        <a
-          class="btn text-white btn-floating m-1"
-          style="background-color: #3b5998;"
-          href="#!"
-          role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="../src/assets/home.png" role="button"
           ><i class="fab fa-facebook-f"></i
         ></a>
   
         <!-- Twitter -->
-        <a
-          class="btn text-white btn-floating m-1"
-          style="background-color: #55acee;"
-          href="#!"
-          role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
           ><i class="fab fa-twitter"></i
         ></a>
   
         <!-- Google -->
-        <a
-          class="btn text-white btn-floating m-1"
-          style="background-color: #dd4b39;"
-          href="#!"
-          role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
           ><i class="fab fa-google"></i
         ></a>
   
         <!-- Instagram -->
-        <a
-          class="btn text-white btn-floating m-1"
-          style="background-color: #ac2bac;"
-          href="#!"
-          role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
           ><i class="fab fa-instagram"></i
         ></a>
   
         <!-- Linkedin -->
-        <a
-          class="btn text-white btn-floating m-1"
-          style="background-color: #0082ca;"
-          href="#!"
-          role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
           ><i class="fab fa-linkedin-in"></i
         ></a>
+  
         <!-- Github -->
-        <a
-          class="btn text-white btn-floating m-1"
-          style="background-color: #333333;"
-          href="#!"
-          role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
           ><i class="fab fa-github"></i
         ></a>
       </section>
